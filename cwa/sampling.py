9a7698b048e1f49b63517f47860acb5a554fc3ce
muriloous <murilomartho@gmail.com>
2025-10-24 20:26:20 -0300

    Change: remove specif sampling functions

--- cwa/sampling.py
+++ cwa/sampling.py
@@ -20,51 +20,16 @@ def sample_from(
         num_context_words: int,
         window_or_max_window_length: int
     ):
+    tokens = prepare_corpus(corpus)
+
+    lowered_tokens = get_lower_tokens(tokens)
+
     match (sample_from):
         case 'context_window':
-            return sample_from_context_window(corpus, num_target_words, num_context_words, window_or_max_window_length)
+            return build_contingency_dicts_from_context_window(lowered_tokens, num_target_words, num_context_words, window_or_max_window_length, with_boundaries = False)
         case 'context_window_with_boundaries':
-            return sample_from_context_window_with_boundaries(corpus, num_target_words, num_context_words, window_or_max_window_length)
+            return build_contingency_dicts_from_context_window(lowered_tokens, num_target_words, num_context_words, window_or_max_window_length, with_boundaries = True)
         case 'sentences':
-            return sample_from_sentences(corpus, num_target_words, num_context_words, window_or_max_window_length)
+            return build_contingency_dicts_from_sentences(lowered_tokens, num_target_words, num_context_words, window_or_max_window_length)
         case _:
             raise Exception(f'"{sample_from}" is not a valid sampling option')
-
-def sample_from_sentences(
-        corpus: str | list[str],
-        num_target_words: int,
-        num_context_words: int,
-        max_window_length: int | list[int]
-    ):
-
-  tokens = prepare_corpus(corpus)
-
-  lowered_tokens = get_lower_tokens(tokens)
-
-  return build_contingency_dicts_from_sentences(lowered_tokens, num_target_words, num_context_words, max_window_length)
-
-def sample_from_context_window(
-      corpus: str | list[str],
-      num_target_words: int,
-      num_context_words: int,
-      window: list[int] | int
-    ):
-
-  tokens = prepare_corpus(corpus)
-
-  lowered_tokens = get_lower_words(tokens)
-
-  return build_contingency_dicts_from_context_window(lowered_tokens, num_target_words, num_context_words, window)
-
-def sample_from_context_window_with_boundaries(
-      corpus: str | list[str],
-      num_target_words: int,
-      num_context_words: int,
-      window: list[int] | int
-    ):
-
-  tokens = prepare_corpus(corpus)
-
-  lowered_tokens = get_lower_tokens(tokens)
-
-  return build_contingency_dicts_from_context_window(lowered_tokens, num_target_words, num_context_words, window)
