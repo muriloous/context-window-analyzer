14b80c86de5fdd75394a94ecacd3d97d9f706183
muriloous <murilomartho@gmail.com>
2025-11-08 14:23:38 -0300

    fix: allow asymmetrical windows in build_contingency_dicts_from_sentences and sort the window list by default in builder functions.

--- cwa/processing/builders.py
+++ cwa/processing/builders.py
@@ -8,13 +8,16 @@ def build_contingency_dicts_from_context_window(
         tokens: list[str],
         num_target_words: int = 300,
         num_context_words: int = 100,
-        window: list[int] = [-2, -1, 1, 2],
+        window: list[int]|int = [-2, -1, 1, 2],
         with_boundaries = False
         ) -> ContingencyTable:
     '''
     Build contingency dicts from the specified context window
     '''
-    if isinstance(window, int): window = [*range(- window, 0), *range(1, window + 1)]
+    if isinstance(window, int):
+        window = [*range(- window, 0), *range(1, window + 1)]
+    else:
+        window = sorted(window)
 
     common_words = get_most_common_words(tokens, max(num_context_words, num_target_words), (not with_boundaries))
     target_words = common_words[:num_target_words]
@@ -41,13 +44,15 @@ def build_contingency_dicts_from_sentences(
         tokens: list[str],
         num_target_words: int = 300,
         num_context_words: int = 100,
-        max_window_length: int = 2
+        window: list[int]|int = [-2, -1, 1, 2],
         ) -> ContingencyTable:
     '''
     Build contingency dicts directly from sentence boundaries
     '''
-    if isinstance(max_window_length, list): max_window_length = abs(max(max_window_length, key=abs))
-    window = [*range(- max_window_length, 0), *range(1, max_window_length + 1)]
+    if isinstance(window, int):
+        window = [*range(- window, 0), *range(1, window + 1)]
+    else:
+        window = sorted(window)
     
     common_words = get_most_common_words(tokens, max(num_context_words, num_target_words), remove_boundary_markers=True)
     target_words = common_words[:num_target_words]
@@ -61,22 +66,25 @@ def build_contingency_dicts_from_sentences(
 
         for inc in [-1, 1]:
             rel_idx = idx + inc
-            idx_distance = 1
+            idx_distance = inc
             while   rel_idx > -1 and \
-                    rel_idx < tokens_list_length and \
-                    idx_distance <= max_window_length:
-                relative_token = tokens[rel_idx]
+                    rel_idx < tokens_list_length:
+                if (inc == -1 and idx_distance < window[0]) or (inc == 1 and idx_distance > window[-1]): break
+                
+                current_rel_idx = rel_idx
+                current_idx_distance = idx_distance
                 rel_idx += inc
-                idx_distance += 1
+                idx_distance += inc
+                if current_idx_distance not in window: continue
 
+                relative_token = tokens[current_rel_idx]
                 if is_boundary_marker(relative_token): break
                 if not relative_token.isalnum(): continue
 
                 context_idx = next((idx for idx, word in enumerate(context_words) if word == relative_token), None)
                 if context_idx == None: continue
 
-                # position_idx = inc * (idx_distance - 1) + max_window_length + (-1 if inc == 1 else 0)
-                position_idx = next((idx for idx, position in enumerate(window) if position == inc * (idx_distance - 1)))
+                position_idx = next((idx for idx, position in enumerate(window) if position == current_idx_distance))
                 contingency_vector_idx = (num_context_words * position_idx) + context_idx
 
                 contingency_table.data[token][contingency_vector_idx] += 1
